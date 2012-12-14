fd81aabcadb3132347c6551aa831f0177e96e674
Władysław Bodzek <wladek@wikia-inc.com>
2012-12-14 12:03:17 +0100

    added basic documentatation of settings.dist.js

--- settings.dist.js
+++ settings.dist.js
@@ -1,18 +1,39 @@
 exports.providers = {
+	/*
+	NAME : {
+		type: PROVIDER_CLASS_NAME,
+		OTHER_SETTINGS_FOR_PROVIDER
+	}
+	*/
 	Test1 : {
 		type: 'Constant',
-		A : 1,
+		A : 1
 	}
 };
 
 exports.sources = {
-
+	/*
+	NAME : {
+		provider: PROVIDER_NAME,
+		data: [ SERIES_NAME_LIST ]
+	}
+	*/
 };
 
 exports.layouts = {
-
+	/*
+	NAME: [{
+		type: WIDGET_CLASS_NAME,
+		sources: [ SOURCE_NAMES_TO_GET_DATA_FROM ]
+		OTHER_SETTINGS_FOR_WIDGET
+	} , ... ]
+	*/
 };
 
 exports.dashboards = {
-
+	/*
+	NAME: {
+		items: [ LAYOUT_NAMES_LIST ]
+	}
+	*/
 };
