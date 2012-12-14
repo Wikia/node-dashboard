bb9fab5bc4fb7b7ef3d5e654ddbf8211e98266d0
Szumo <szumo@wikia-inc.com>
2012-12-14 11:18:46 +0100

    Tests updated

--- test/test-sources.js
+++ test/test-sources.js
@@ -2,11 +2,11 @@ sources = require("./../lib/sources")
 providers = require("./../lib/providers")
 
 exports.test_provider = function(beforeExit, assert) {
-    provider = providers.Constant( { A: 5, B: 6, C:7 } )
+    provider = providers.Constant( { values: { A: 5, B: 6, C:7 } })
     data = provider.getData();
     assert.equal(data.A.length, 1);
     assert.equal(data.A[0][1], 5);
-    src = sources.Source();
+    src = new sources.Source();
     assert.isDefined(src);
     provider.addSubscriber(src, ['A', 'B', 'C']);
     provider.start();
