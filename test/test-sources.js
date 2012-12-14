f70b01c8261256e97dc58b36162a6876e6bc2dd4
Szumo <szumo@wikia-inc.com>
2012-12-14 13:11:45 +0100

    Start of the NewRelic provider

--- test/test-sources.js
+++ test/test-sources.js
@@ -1,8 +1,10 @@
-sources = require("./../lib/sources")
-providers = require("./../lib/providers")
+sources = require("./../lib/sources");
+providers = require("./../lib/providers");
+util = require("util");
 
-exports.test_provider = function(beforeExit, assert) {
-    provider = providers.Constant( { values: { A: 5, B: 6, C:7 } })
+
+exports.const_provider = function(beforeExit, assert) {
+    provider = new providers.Constant( { values: { A: 5, B: 6, C:7 } })
     data = provider.getData();
     assert.equal(data.A.length, 1);
     assert.equal(data.A[0][1], 5);
@@ -12,4 +14,21 @@ exports.test_provider = function(beforeExit, assert) {
     provider.start();
     last = src.getLast();
     assert.eql(last, {A: 5, B:6, C:7})
-}
+};
+
+exports.new_relic = function(beforeExit, assert) {
+  settings = {
+		type: 'NewRelic',
+		apiKey: process.env.NEWRELIC_API_KEY,
+		accountId: process.env.NEWRELIC_ACCOUNT,
+		applications: undefined
+	}
+  nr = new providers.NewRelic(settings)
+  nr.fetchApplications(util.log)
+
+
+
+
+
+
+};
